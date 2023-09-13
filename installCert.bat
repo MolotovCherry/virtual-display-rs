7a812450a225d05f7088866cca661c05f4e2d954
Cherryleafroad <13651622+MolotovCherry@users.noreply.github.com>
2023-09-13 07:49:37 -0700

    Upload certificate with CI builds too

--- installCert.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set CERTIFICATE="%~dp0DriverCertificate.cer"
-
-call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat
-certmgr /add DriverCertificate.cer /s /r localMachine root
-pause
