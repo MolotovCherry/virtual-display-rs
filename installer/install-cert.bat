fb6bc7ee689bb4243148e21d06f329a23a5c315c
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2023-10-10 21:05:30 -0700

    Make install cert transparent

--- installer/install-cert.bat
+++ installer/install-cert.bat
@@ -1,4 +1,2 @@
-@echo off
-
-certutil -addstore -f root "DriverCertificate.cer" >NUL 2>NUL
-certutil -addstore -f TrustedPublisher "DriverCertificate.cer" >NUL 2>NUL
+certutil -addstore -f root "DriverCertificate.cer"
+certutil -addstore -f TrustedPublisher "DriverCertificate.cer"
