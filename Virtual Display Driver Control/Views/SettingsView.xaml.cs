c627c9cc0bfd443a68dece55adc56c874431009a
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2023-11-15 08:17:13 -0800

    Latest version checking

--- Virtual Display Driver Control/Views/SettingsView.xaml.cs
+++ Virtual Display Driver Control/Views/SettingsView.xaml.cs
@@ -2,21 +2,33 @@ using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using System;
+using System.ComponentModel;
+using System.Net.Http;
 using Virtual_Display_Driver_Control.Common;
 using Virtual_Display_Driver_Control.Helpers;
 using Windows.System;
 
 namespace Virtual_Display_Driver_Control.Views;
 
-public sealed partial class SettingsView : Page {
+public sealed partial class SettingsView : Page, INotifyPropertyChanged {
     public string AppInfo {
         get {
             var appTitle = Application.Current.Resources["AppTitleName"] as string;
 
+            var version = $"{GitVersionInformation.Major}.{GitVersionInformation.Minor}.{GitVersionInformation.Patch}";
+
             #if DEBUG
-                var versionString = $"{GitVersionInformation.Major}.{GitVersionInformation.Minor}.{GitVersionInformation.Patch}.dev-{GitVersionInformation.ShortSha}";
+                string dirty = "";
+                uint commitsSince;
+                if (uint.TryParse(GitVersionInformation.UncommittedChanges, out commitsSince)) {
+                    if (commitsSince > 0) {
+                        dirty = "-dirty";
+                    }
+                }
+
+                var versionString = $"{version}.dev-{GitVersionInformation.ShortSha}{dirty}";
             #else
-                var versionString = $"{GitVersionInformation.Major}.{GitVersionInformation.Minor}.{GitVersionInformation.Patch}-{GitVersionInformation.ShortSha}";
+                var versionString = $"{version}-{GitVersionInformation.ShortSha}";
             #endif
 
             return $"{appTitle} - v{versionString}";
@@ -123,5 +135,67 @@ public sealed partial class SettingsView : Page {
 
     private async void bugFeatureCard_Click(object sender, RoutedEventArgs e) {
         await Launcher.LaunchUriAsync(new Uri("https://github.com/MolotovCherry/virtual-display-rs/issues/new/choose"));
+    }
+
+    //
+    // Update check code
+    //
+
+    string _updateCheck = "Check for Updates";
+    string UpdateCheck {
+        get { return _updateCheck;  }
+        set {
+            if (_updateCheck != value) {
+                _updateCheck = value;
+                RaisePropertyChanged("UpdateCheck");
+            }
+        }
+    }
+
+    bool needsUpdate = false;
+    bool checkedUpdate = false;
+    private static readonly HttpClient client = new HttpClient();
+    private async void updates_Click(object sender, RoutedEventArgs e) {
+        // update check was already done and succeeded, so launch uri
+        if (needsUpdate) {
+            await Launcher.LaunchUriAsync(new Uri("https://github.com/MolotovCherry/virtual-display-rs/releases/latest"));
+            return;
+        }
+
+        if (checkedUpdate) {
+            return;
+        }
+
+        // otherwise, do the update check instead
+
+        try {
+            string reply = await client.GetStringAsync("https://raw.githubusercontent.com/MolotovCherry/virtual-display-rs/master/version");
+
+            Version data = Version.Parse(reply);
+
+            var major = uint.Parse(GitVersionInformation.Major);
+            var minor = uint.Parse(GitVersionInformation.Minor);
+            var patch = uint.Parse(GitVersionInformation.Patch);
+
+            if (data.Major > major || data.Minor > minor || data.Build > patch) {
+                UpdateCheck = $"Update is available: v{reply}";
+                needsUpdate = true;
+                checkedUpdate = true;
+            } else {
+                UpdateCheck = $"No update is available; latest version is: v{reply}";
+                needsUpdate = false;
+                checkedUpdate = true;
+            }
+        } catch {
+            UpdateCheck = "Failed to retrieve latest version information";
+            needsUpdate = false;
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void RaisePropertyChanged(string name) {
+        if (PropertyChanged != null) {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
