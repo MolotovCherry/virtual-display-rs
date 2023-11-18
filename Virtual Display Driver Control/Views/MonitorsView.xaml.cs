5e8a77307a467923d0d6658495413da03a9c7c8e
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2023-11-17 21:20:10 -0800

    Convert to clrf

--- Virtual Display Driver Control/Views/MonitorsView.xaml.cs
+++ Virtual Display Driver Control/Views/MonitorsView.xaml.cs
@@ -1,22 +1,55 @@
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
-namespace Virtual_Display_Driver_Control.Views; 
+namespace Virtual_Display_Driver_Control.Views;
 
 public sealed partial class MonitorsView : Page {
+    private static Ipc? _ipc = null;
+
+    static bool s_IsConnected {
+        get {
+            return _ipc != null && Ipc.IsConnected;
+        }
+    }
+
+    static Ipc? s_Ipc {
+        get {
+            if (s_IsConnected) {
+                return _ipc;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    static List<Monitor>? savedData = null;
+    static List<Monitor>? monitorList = null;
+
     public MonitorsView() {
-        this.InitializeComponent();
+        InitializeComponent();
+
+        // Now initialize Ipc since we have defined the callback
+        Ipc.GetOrCreateIpc((Ipc ipc) => {
+            System.Diagnostics.Debug.WriteLine("Init view");
+            savedData = ipc.RequestState();
+            // an actual clone, we don't want to touch the original savedData
+            monitorList = savedData.Select(monitor => (Monitor)monitor.Clone()).ToList();
+            _ipc = ipc;
+        });
+    }
+
+    // callback is fired once it reconnects (if it does). can be null if not desired
+    public void ReconnectIpc(Action<Ipc> callback) {
+        if (!s_IsConnected) {
+            Ipc.GetOrCreateIpc((Ipc ipc) => {
+                _ipc = ipc;
+
+                if (callback != null) {
+                    callback(ipc);
+                }
+            }, null);
+        }
     }
 }
