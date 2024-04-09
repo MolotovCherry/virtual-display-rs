b1c69634e43afec7ff1cea8cce10b8e57ac8e596
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-04-09 10:40:04 -0700

    Hook up driver ipc bindings to dart api

--- rust/bindings/dart_vdd/rust/src/api.rs
+++ rust/bindings/dart_vdd/rust/src/api.rs
@@ -1,3 +1,4 @@
+use driver_ipc::{ClientCommand, Dimen, DriverClient, EventCommand, Id, RefreshRate, Result};
 pub use driver_ipc::{Mode, Monitor};
 use flutter_rust_bridge::frb;
 
@@ -5,7 +6,7 @@ use crate::frb_generated::StreamSink;
 
 #[frb(mirror(Monitor), dart_metadata=("freezed"))]
 pub struct _Monitor {
-    pub id: u32,
+    pub id: Id,
     pub name: Option<String>,
     pub enabled: bool,
     pub modes: Vec<Mode>,
@@ -13,29 +14,30 @@ pub struct _Monitor {
 
 #[frb(mirror(Mode), dart_metadata=("freezed"))]
 pub struct _Mode {
-    pub width: u32,
-    pub height: u32,
-    pub refresh_rates: Vec<u32>,
+    pub width: Dimen,
+    pub height: Dimen,
+    pub refresh_rates: Vec<RefreshRate>,
 }
 
 #[frb(opaque)]
-pub struct VirtualDisplayDriver {}
+pub struct VirtualDisplayDriver {
+    client: DriverClient,
+}
 
 impl VirtualDisplayDriver {
     #[frb(sync)]
-    pub fn new(pipe_name: Option<String>) -> VirtualDisplayDriver // Result<VirtualDisplayDriver, ...>
-    {
-        VirtualDisplayDriver {}
+    pub fn new(_pipe_name: Option<String>) -> Result<VirtualDisplayDriver> {
+        let vdd = VirtualDisplayDriver {
+            client: DriverClient::new()?,
+        };
+
+        Ok(vdd)
     }
 
     /// Get the current state of the driver.
     #[frb(getter)]
-    pub fn state(&self) -> Vec<Monitor> // -> Result<Vec<Monitor>, ...>
-    {
-        // Can return std::result::Result or anyhow::Result. The latter will be
-        // converted into a generic AnyhowException on the Dart side. The Error
-        // trait is ignored. eyre::Result does not work.
-        todo!("state")
+    pub fn state(&self) -> Vec<Monitor> {
+        self.client.monitors().to_owned()
     }
 
     /// Stream of state changes.
@@ -46,51 +48,84 @@ impl VirtualDisplayDriver {
     ///
     /// After calling, will instantly emit the current state of the driver.
     #[frb(getter)]
-    pub fn stream(&self, sink: StreamSink<Vec<Monitor>>) // -> Result<(), ...>
-    {
-        todo!("stream")
+    pub fn stream<F: FnOnce() + Send + 'static>(&self, sink: StreamSink<Vec<Monitor>>) {
+        self.client.set_receiver(None::<F>, move |command| {
+            if let ClientCommand::Event(EventCommand::Changed(data)) = command {
+                if let Err(_e) = sink.add(data) {
+                    // do something with err? hmm
+                }
+            }
+        });
     }
 
     /// Set the state of the provided monitors.
     ///
     /// Each monitor with a matching ID will be updated to the provided state.
-    pub fn set_monitors(&self, monitors: Vec<Monitor>) // -> Result<(), ...>
-    {
-        // assert only existing monitors
-        todo!("set_monitors")
+    pub fn set_monitors(&mut self, monitors: &[Monitor]) -> Result<()> {
+        self.client.set_monitors(&monitors)
     }
 
     /// Set the state of the monitor with the provided ID.
     ///
     /// Only the provided properties will be updated.
     pub fn set_monitor(
-        &self,
+        &mut self,
         id: u32,
         enabled: Option<bool>,
         name: Option<String>,
         modes: Option<Vec<Mode>>,
-    ) // -> Result<(), ...>
-    {
-        // assert only existing monitor
-        todo!("set_monitor_enabled")
+    ) -> Result<()> {
+        self.client.find_monitor_mut(id, |monitor| {
+            if let Some(enabled) = enabled {
+                monitor.enabled = enabled;
+            }
+
+            monitor.name = name;
+
+            if let Some(modes) = modes {
+                monitor.modes = modes;
+            }
+        })?;
+
+        Ok(())
     }
 
     /// Add a new monitor to the driver.
-    pub fn add_monitor(&self, name: Option<String>, enabled: bool, modes: Vec<Mode>)
-    // -> Result<(), ...>
-    {
-        todo!("add_monitor")
+    pub fn add_monitor(
+        &mut self,
+        name: Option<String>,
+        enabled: bool,
+        modes: Vec<Mode>,
+    ) -> Result<()> {
+        let monitor = Monitor {
+            id: self.client.new_id(None)?,
+            name,
+            enabled,
+            modes,
+        };
+
+        self.client.add(monitor);
+
+        Ok(())
     }
 
     /// Remove monitors from the driver.
-    pub fn remove_monitors(&self, ids: Vec<u32>) // -> Result<(), ...>
-    {
-        todo!("remove_monitors")
+    pub fn remove_monitors(&mut self, ids: &[u32]) {
+        self.client.remove(&ids);
     }
 
     /// Remove all monitors from the driver.
-    pub fn remove_all_monitors(&self) // -> Result<(), ...>
-    {
-        todo!("remove_all_monitors")
+    pub fn remove_all_monitors(&mut self) {
+        self.client.remove_all();
+    }
+
+    /// Push in-memory changes to driver.
+    pub fn notify(&mut self) -> Result<()> {
+        self.client.notify()
+    }
+
+    /// Persist in-memory changes to user settings
+    pub fn persist(&mut self) -> Result<()> {
+        self.client.persist()
     }
 }
